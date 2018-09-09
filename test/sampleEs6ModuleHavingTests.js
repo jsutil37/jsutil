a799db03d1a2744822be79ecc59b2470e6a53599
jsutil37 <jsutil37@gmail.com>
2018-09-09 09:57:43 +0530

    updated

---
+++ test/sampleEs6ModuleHavingTests.js
@@ -0,0 +1,17 @@
+function f1()
+{
+    return true
+}
+
+function f2()
+{
+    return false
+}
+
+export const tests =
+[
+    {
+        "passing sample test fn":f1,
+        "failing sample test fn":f2
+    }
+]
