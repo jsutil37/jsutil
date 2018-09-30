1dd626445de81627c57bd2c5f20e3b0056721a91
jsutil37 <jsutil37@gmail.com>
2018-09-30 23:57:16 +0530

    temp debug log enabled to debug strange multiple jquery issue

--- loaderUtil.js
+++ loaderUtil.js
@@ -121,7 +121,7 @@ export async function checkAndRecordUrl(url)
 
 async function loadScript2(url,integrity,crossOrigin,scriptType)
 {
-	//console.log('loadScript(): using promises...url=\''+url+'\'...')
+	console.log('loadScript(): using promises...url=\''+url+'\'...')
 	return new Promise
 	(
 	function (callbackfn)
