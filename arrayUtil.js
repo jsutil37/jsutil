eb6cf0690349439f3643e8b8071daf450f06220b
jsutil37 <42505522+jsutil37@users.noreply.github.com>
2020-04-17 21:47:09 +0530

    Update arrayUtil.js

--- arrayUtil.js
+++ arrayUtil.js
@@ -1,2 +1,3 @@
 Array.prototype.removeFirstEle=function(){return this.shift()}
 Array.prototype.lastEle=function(){return this[this.length-1]}
+Array.prototype.removeLastEle=function(){return this.pop()}
