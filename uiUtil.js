bb367edb35aa032d49406d3bd352c3924d557e90
jsutil37 <42505522+jsutil37@users.noreply.github.com>
2021-02-28 18:03:06 +0530

    Update uiUtil.js

--- uiUtil.js
+++ uiUtil.js
@@ -1,4 +1,7 @@
 export {randLightColor}
+
+import {randEl} from './util.js'
+
 function randLightColor() {
 	let letters = 'BCDEF'.split(''), color = '#'
 	for (let i = 0; i < 6; i++) { color += randEl(letters) }
