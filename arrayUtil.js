22a136be840c387e5090b63642112c2688cba9a9
jsutil37 <42505522+jsutil37@users.noreply.github.com>
2021-03-17 01:04:22 +0530

    Update arrayUtil.js

--- arrayUtil.js
+++ arrayUtil.js
@@ -1,4 +1,14 @@
 //TODO: convert to (or at least add) non-prototype-changing forms
+
+//copy-paste assert function into this file, so as to prevent circular dependencies (bad hack, TODO: if needed create a basic debug util file):
+function assert(x, errMsg) {
+	if(x){return}
+	let s =(errMsg == null)?'assert failed':"Assert Failed\n"+errMsg
+	debugger
+	a(s)
+	throw new Error(s)
+}
+
 assert(Array.prototype.removeFirstEle==null)
 Array.prototype.removeFirstEle=function(){return this.shift()}
 
