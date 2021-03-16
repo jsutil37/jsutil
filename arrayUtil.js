4d3f25f53a7aa91f3767d43faffaf7feea9864b6
jsutil37 <42505522+jsutil37@users.noreply.github.com>
2021-03-16 23:13:19 +0530

    Update arrayUtil.js

--- arrayUtil.js
+++ arrayUtil.js
@@ -1,3 +1,16 @@
+//TODO: convert to (or at least add) non-prototype-changing forms
+assert(Array.prototype.removeFirstEle==null)
 Array.prototype.removeFirstEle=function(){return this.shift()}
+
+assert(Array.prototype.lastEle==null)
 Array.prototype.lastEle=function(){return this[this.length-1]}
+
+assert(Array.prototype.removeLastEle==null)
 Array.prototype.removeLastEle=function(){return this.pop()}
+
+assert(Array.prototype.asyncForEach==null)
+Array.prototype.asyncForEach = async function (fn) {
+    for (let i = 0; i < this.length; i++) {
+      await fn(this[i], i);
+    }
+}
