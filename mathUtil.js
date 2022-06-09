ce6c2a1b7bedab5733b66d674fcb4ea1b98b3830
jsutil37 <42505522+jsutil37@users.noreply.github.com>
2022-06-09 10:49:35 +0530

    Update mathUtil.js

--- mathUtil.js
+++ mathUtil.js
@@ -1,6 +1,7 @@
 //math utilities including arbitrary precision math
 export {
 	wholeIntTo32bitBinaryString,
+	wholeIntTo32bitArray,
 	stringToBinaryStringPrefixedWithItsLength,
 	ThirtyTwoBitBinaryStringToWholeInt,
 	stringToBinaryString,
@@ -20,6 +21,21 @@ function wholeIntTo32bitBinaryString(i) {
 		rv = '0' + rv;
 	}
 	return rv;
+}
+
+/**
+ * @param {number} i
+ */
+function wholeIntTo32bitArray(i) {
+  const str = wholeIntTo32bitBinaryString(i);
+  const output = [];
+  assert(str.length == 32);
+  for (let j = 0; j < 32; j++) {
+    const bitChr = str.charAt(j);
+    assert(["0", "1"].includes(bitChr));
+    output.push(bitChr == "1");
+  }
+  return output;
 }
 
 function stringToBinaryStringPrefixedWithItsLength(s) {
