17047c508318237a95529616c476ccb3478d0b86
jsutil37 <42505522+jsutil37@users.noreply.github.com>
2023-03-08 10:04:48 +0530

    Update domUtil.js

--- domUtil.js
+++ domUtil.js
@@ -500,12 +500,13 @@ window.htmlEncode = htmlEncode;
  * @return {Element}
  * reference: https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
  */
-window.htmlToElement = function (html) {
+export function htmlToElement(html) {
   var template = document.createElement("template");
   html = html.trim(); // Never return a text node of whitespace as the result
   template.innerHTML = html;
   return template.content.firstChild;
 };
+window.htmlToElement = htmlToElement;
 
 window.redrawHtml = function (html, parentEle) {
   var noSpaceHtml = html.replaceAll(" ", "");
